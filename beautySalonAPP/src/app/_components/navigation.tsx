4c9a8f51e7b540e5b974a952f56c19b68bbab239
Kacper <kdeja.webdev@gmail.com>
2025-03-11 18:23:51 +0100

    footer initial design. 1.10

--- beautySalonAPP/src/app/_components/navigation.tsx
+++ beautySalonAPP/src/app/_components/navigation.tsx
@@ -2,7 +2,7 @@
 export const Navigation = () => (
     <div className="p-5 flex justify-around bg-stone-200">
         <div className="w-1/5 px-10">
-            <span className="text-sm font-extrabold">SALOONIK</span>
+            <span className="text-md font-extrabold">SALOONIK</span>
         </div>
         <span>Home</span>
         <span>Features</span>
